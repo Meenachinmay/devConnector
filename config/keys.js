5b94e120c5c60a332263a2653af35322b0ee10d6
meenachinmay <chinmayanand896@gmail.com>
2018-09-28 04:46:39 +0530

    config updated

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     mongoURI: 'mongodb://meenachinmay:meena1234@ds115963.mlab.com:15963/devconnector_demo',
     secretOrKey: 'secret'
-};
-=======
-    mongoURI: 'mongodb://<username>:<password>@ds115963.mlab.com:15963/devconnector_demo'
-};
->>>>>>> 70444ff537f04c480f0fc963f3f6018319149c23
+}
